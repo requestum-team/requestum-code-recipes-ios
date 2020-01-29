eacb8b363ea4038aead2f33cc45ecefa64870767
Alex Kovalov <alex.kovalov@requestum.com>
2020-01-29 14:32:05 +0200

    Add scrollview keyboard offset handling

---
+++ Extensions/UIKit/UIScrollView+Keyboard.swift
@@ -0,0 +1,41 @@
+//
+//  UIScrollView+Keyboard.swift
+//  TownApp
+//
+//  Created by Alex Kovalov on 23.01.2020.
+//  Copyright © 2020 Requestum. All rights reserved.
+//
+
+import UIKit
+
+extension UIScrollView {
+    
+    func setupKeyboardNotifications() {
+        
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+    }
+    
+    @objc
+    func adjustForKeyboard(notification: Notification) {
+        
+        guard let keyboardValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else {
+            return
+        }
+        guard let view = superview else {
+            return
+        }
+
+        let keyboardScreenEndFrame = keyboardValue.cgRectValue
+        let keyboardViewEndFrame = view.convert(keyboardScreenEndFrame, from: view.window)
+
+        if notification.name == UIResponder.keyboardWillHideNotification {
+            contentInset = UIEdgeInsets(top: 0, left: 0, bottom: view.safeAreaInsets.bottom, right: 0)
+        } else {
+            contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height - view.safeAreaInsets.bottom, right: 0)
+        }
+
+        scrollIndicatorInsets = contentInset
+    }
+}
